83636d3163ba4018fc80be8a594367064ad2dd07
Jim Katz <jimk@tbrc.org>
2022-08-29 13:01:44 -0400

    ao-workflows-2 Remediate IA

--- IARemediate/iacrawl.py
+++ IARemediate/iacrawl.py
@@ -11,37 +11,54 @@ Workflow for https://github.com/buda-base/ao-workflows/issues/2
 """
 from pathlib import Path
 
-from dagster import define_asset_job, job, asset, repository
+from dagster import define_asset_job, job, asset, repository, materialize, get_dagster_logger
 
 from iaCrawlUtils import ia_report_log, ia_lib
 
 
-@asset
+@asset(group_name='ia_crawl')
 def mismatch_data():
     log = ia_report_log(Path(Path.home() / "dev" / "ao-workflows" / "data" / "ia-mismatch.report.log"))
+    # log = ia_report_log(Path(Path.home() / "dev-from-deploy" / "ao-workflows" / "data" / "ia-mismatch.report.log"))
     results = log.raw_lines
+    get_dagster_logger().info(f"loaded {len(results)} lines from log")
     return results
 
 
-@asset
+@asset(group_name='ia_crawl')
 def mismatches(mismatch_data):
+    """
+    Turns raw mismatch data into data structure
+    :param mismatch_data: [textline, ..... ]
+    :return: { reason: [item_id, ....] }
+    """
     results: {} = {}
     cur_reason: str = 'base reason:'
     results[cur_reason] = set()
+    total_mismatches: int = 0
     for _line in mismatch_data:
         if _line.startswith('bdrc'):
             results[cur_reason].add(_line)
+            total_mismatches += 1
         else:
             # add a new reason
             cur_reason = _line
             results[cur_reason] = set()
+
+    get_dagster_logger().info(f"Calculated {len(results)} reasons, {total_mismatches} works")
     return results
 
 
-@asset
+@asset(group_name='ia_crawl')
 def item_mismatches(mismatches):
+    """
+    Iterates mismatches
+    :param mismatches: {reason : [item id, ....], .... }
+    :return: { item_id: [reason, ..... ] }
+    """
     results: {} = {}
 
+    total_work_mismatches: int = 0
     # items() is an iterator of unnamed tuples
     for item in mismatches.items():
         k = item[0]
@@ -49,48 +66,81 @@ def item_mismatches(mismatches):
             if w not in results.keys():
                 results[w] = set()
             results[w].add(k)
+            total_work_mismatches += 1
+    get_dagster_logger().info(f"found {len(results)} mismatched items, with a total of {total_work_mismatches} reasons")
     return results
 
 
-@asset
+@asset(group_name='ia_crawl')
 def items_with_failed_derive(item_mismatches):
     """
     Get the last derive step for each item. If it failed its last derive,
     Add it to the list
-    :param item_mismatches: mismatched inversion, keyed by item number
-    :return:
+    :param item_mismatches:  { item_id: [reason, ..... ] }
+    :return: [item IA id,...]
     """
     ia_sess: ia_lib = ia_lib()
     y = []
-    iTries = 0
     for x in item_mismatches.items():
         if ia_sess.get_last_work_derive_task(x[0]).color is not None:
-            y.append(x)
-        iTries += 1
-        if iTries == 10:
-            break
+            y.append(x[0])
+        # if len(y) > 10:
+        #     break
+
+    get_dagster_logger().info(f"found {len(y)} items that failed derive.")
     return y
 
-
-mismatch_job = define_asset_job(name="get_raw_mismatches_job", selection="mismatch_data")
-items_for_mismatch = define_asset_job(name="mismatched_items", selection="mismatches")
-mismatch_for_items = define_asset_job(name="mismatches_for_items", selection="item_mismatches")
-
-
-@job
-def some_ia_action():
+@asset(group_name='ia_crawl')
+def mismatched_items_that_derived(item_mismatches, items_with_failed_derive):
     """
-    Stub - not called directly
-    :return:
+    returns the items which were mismatched and also did derive
+    :param item_mismatches: [{ item_id: [reasons,...]}
+    :param items_with_failed_derive: [item_id,...]
     """
-    x = items_with_failed_derive(item_mismatches(mismatches(mismatch_data())))
+    mismatched_as_set = set(item_mismatches.keys())
+    failed_derive_set = set(items_with_failed_derive)
+    results  = list (mismatched_as_set - failed_derive_set)
+    get_dagster_logger().info(f"found {len(results)} mismatches where derive was successful.")
+
+
+#
+# mismatch_job = define_asset_job(name="get_raw_mismatches_job", selection="mismatch_data")
+# items_for_mismatch = define_asset_job(name="mismatched_items", selection="mismatches")
+# mismatch_for_items = define_asset_job(name="mismatches_for_items", selection="item_mismatches")
+
+#
+# @job
+# def some_ia_action():
+#     """
+#     Stub - not called directly
+#     :return:
+#     """
+#     x = items_with_failed_derive(item_mismatches(mismatches(mismatch_data())))
+#
 
 
 @repository
 def ia_fix_repo():
-    return [some_ia_action, mismatch_job, items_for_mismatch, mismatch_for_items,
-            mismatch_data, mismatches, item_mismatches]
+    # return [some_ia_action, mismatch_job, items_for_mismatch, mismatch_for_items,
+    #         mismatch_data, mismatches, item_mismatches]
+    #
+    # defining one job for all assets
+    return [mismatch_data, mismatches, item_mismatches, items_with_failed_derive, mismatched_items_that_derived]
 
 
 if __name__ == '__main__':
-    some_ia_action.execute_in_process()
+    # mismatch_for_items.ex
+    assets = [
+        mismatch_data,
+        mismatches,
+        item_mismatches,
+        items_with_failed_derive,
+        mismatched_items_that_derived,
+    ]
+
+    get_dagster_logger().info(f"materializing")
+
+    # Rebuild from scratch
+    result = materialize(assets)
+    # Try - nope: says upstream not materialized
+    # results = materialize([mismatched_items_that_derived])
