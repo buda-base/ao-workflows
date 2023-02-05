e910e66e8f3bd7582ae678828e2370476d50c5ad
Jim Katz <jimk@tbrc.org>
2023-02-05 10:40:05 -0500

    WIP - generate work ids
    ao-google-books-59 Populate Google Books worklist

--- GoogleBooks/GbWorkflowUtils.py
+++ GoogleBooks/GbWorkflowUtils.py
@@ -4,6 +4,19 @@ Helper utilities for workflow
 import requests
 from requests import Response
 
+def dequote(s: str) -> str:
+    """
+    If a string has single or double quotes around it, remove them.
+    Make sure the pair of quotes match.
+    If a matching pair of quotes is not found,
+    or there are less than 2 characters, return the string unchanged.
+    [thanks to stackoverflow](https://stackoverflow.com/questions/3085382/how-can-i-strip-first-and-last-double-quotes)
+    """
+    if (len(s) >= 2 and s[0] == s[-1]) and s.startswith(("'", '"')):
+        return s[1:-1]
+    return s
+
+
 # Can import as csv_f, json, or simple csv (one column)
 BUDA_Get_GB_ReadyUrl: str = "https://purl.bdrc.io/query/table/AO_okforGB?&format=csv&pageSize=20000"
 buda_work_prefix:str = "bdr:"
@@ -33,10 +46,20 @@ def transform_works(in_list:[str])-> int:
     :return: for every element containing bdr: return the work Id that goes with that work.
     Creates the work if needed
     """
+    # nasty, filthy, dirty input
+    in_list = [dequote(x) for x in in_list]
+
     # 1. Remove everything that's not a work, and remove the prefix
     work_elems = [ x[len(buda_work_prefix):] for x in in_list if x.startswith(buda_work_prefix)]
+    work_list_ids:[int] = []
+    from BdrcDbLib.DbOrm.DrsContextBase import DrsDbContextBase
+    with DrsDbContextBase() as ctx:
+        work_list_ids = [ x.workId for x in [ ctx.get_or_create_work(y) for y in work_elems ]]
+
+    return work_list_ids
+
 
 
 # Just invoke for testing
 if __name__ == '__main__':
-    print(len(get_gb_candidates()))
+    print(transform_works(get_gb_candidates()))
