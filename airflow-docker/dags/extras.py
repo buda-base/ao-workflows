de75e3744fa43cb0eaeb8acbe95282f59d88724c
Jim Katz <jimk@tbrc.org>
2024-05-12 10:32:54 -0400

    Add from list

--- airflow-docker/dags/extras.py
+++ airflow-docker/dags/extras.py
@@ -1,7 +1,11 @@
 """
 These are outside of the dags. These are one-time routines that use GlacierSyncProgress
 """
+import argparse
 from datetime import time
+from time import sleep
+
+import pendulum
 
 from GlacierSyncProgress import GlacierSyncProgress
 from BdrcDbLib.DbOrm.DrsContextBase import DrsDbContextBase
@@ -39,11 +43,10 @@ def glacier_sync_catchup():
     with DrsDbContextBase() as drs:
         sess = drs.get_session()
         for work in done_already:
-            time.sleep(1)
+            sleep(1)
             import pendulum
             xnow = pendulum.now()
-            gsp = GlacierSyncProgress()
-            gsp.object_name = work
+            gsp_w = sess.query(GlacierSyncProgress).filter(GlacierSyncProgress.object_name == work).one()
             gsp.restore_complete_on = xnow
             gsp.download_complete_on = xnow
             gsp.debag_complete_on = xnow
@@ -51,4 +54,32 @@ def glacier_sync_catchup():
             gsp.update_user_data(xnow.to_rfc3339_string() ,{'work_rid': work, 'provider': 'manual'})
             sess.add(gsp)
             sess.commit()
-            print(f"{work.work_rid} done")
+            print(f"{work} done")
+
+def add_works_to_project():
+    """
+    read a list from a csv and add them to the GlacierSyncProgress table, with NO activities logged
+    :return:
+    """
+    ap = argparse.ArgumentParser()
+    ap.add_argument('work_list', help='a csv file with a list of works to add to the GlacierSyncProgress table',type=args.FileType('r'))
+
+    args = ap.parse_args()
+    with args.work_list as f:
+        csvr = csv.reader(f)
+        with DrsDbContextBase() as drs:
+                sess = drs.get_session()
+                # Open a csv reader from f
+                # read each line and add it to the GlacierSyncProgress table
+                for row in csvr:
+                    work = row[0]
+                    aws_s3_path=row[1]
+                    gsp = GlacierSyncProgress()
+                    gsp.object_name = work
+                    gsp.update_user_data( pendulum.now().to_rfc3339_string(), {'aws_s3_path': aws_s3_path})
+                    sess.add(gsp)
+                    sess.commit()
+                    print(f"{work=}, {aws_s3_path=} added")
+
+if __name__ == '__main__':
+    add_works_to_project()
